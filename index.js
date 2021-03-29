6d60dcdfd9fd3b838996dc20f02e3b985e37d735
Jose Luis Garcia M <jl.garcia@ardana.com.mx>
2021-03-29 16:16:05 -0600

    Archivos estaticos y rutas

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+
+const path = require('path');
+
+const router = require('./routes/index');
 
 const app = express();
 
-app.use('/', (req, res) => {
-    res.send('funcionando');
-})
+app.engine('handlebars', 
+    exphbs({
+        defaultLayout: 'layout'
+    })
+);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/', router());
 
 app.listen(8080);
