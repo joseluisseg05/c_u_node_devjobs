c69b0addd82a2751aec4b29559ac73ef5fe5326e
Jose Luis Garcia M <jl.garcia@ardana.com.mx>
2021-03-30 12:38:54 -0600

    Mongo config

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+const mongoose = require('mongoose');
+require('./config/db');
+
 const express = require('express');
 const exphbs = require('express-handlebars');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
 const path = require('path');
+
+require('dotenv').config({path: '.env'})
 
 const router = require('./routes/index');
 
@@ -14,8 +22,21 @@ app.engine('handlebars',
 );
 app.set('view engine', 'handlebars');
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cookieParser());
+
+app.use(session({
+    secret: process.env.SECRETO,
+    key: process.env.KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+        mongoUrl: process.env.DATABASE,
+        touchAfter: 24 * 3600
+    })
+}))
 
 app.use('/', router());
 
-app.listen(8080);
+app.listen(process.env.PUERTO);
